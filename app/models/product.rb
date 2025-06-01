1e6193e1a280fabafda5e3e4a98de41dbd69090c
John Rezaik <johnrezaik@gmail.com>
2025-06-01 18:07:26 +0300

    Rails lab-3 ,Products & auth

---
+++ app/models/product.rb
@@ -0,0 +1,4 @@
+class Product < ApplicationRecord
+    validates :name, presence: true
+  # , uniqueness: { case_sensitive: false }, length: {maximum: 50}, format: { with: ConstantData::VALID_EMAIL_REGEX }, if: :method_name?
+end
